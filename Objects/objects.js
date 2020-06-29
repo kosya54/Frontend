a75ea282d6bec995d665aac078c520d561526fcd
kosya54 <kosya54@gmail.com>
2020-06-30 04:10:02 +0700

    Исправление замечаний CSS Text

---
+++ Objects/objects.js
@@ -0,0 +1,54 @@
+(function () {
+    var country = [
+        {
+            name: "Russia",
+            cities: [{
+                name: "Moscow",
+                population: 11920000
+            }, {
+                name: "Novosibirsk",
+                population: 1511000
+            }, {
+                name: "Kazan",
+                population: 1169000
+            }]
+        }, {
+            name: "Japan",
+            cities: [{
+                name: "Gamagori",
+                population: 80693
+            }, {
+                name: "Shinshiro",
+                population: 47527
+            }, {
+                name: "Nisshin",
+                population: 88419
+            }]
+        }, {
+            name: "Commonwealth of Australia",
+            cities: [{
+                name: "Ballarat",
+                population: 101588
+            }, {
+                name: "Canberra",
+                population: 410301
+            }, {
+                name: "Perth",
+                population: 2059484
+            }, {
+                name: "Meekatharra",
+                population: 708
+            }]
+        }
+    ];
+
+    var countryMaxCity = [];
+    var countCities = 0;
+    country.forEach(function (elem) {
+
+    });
+
+    console.log(countryMaxCity.toString());
+
+}());
+
