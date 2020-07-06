286cfc9ffea822205f52bff64e61e0e53ea86e08
kosya54 <49726304+kosya54@users.noreply.github.com>
2020-07-06 12:56:20 +0300

    Update arrays.js

--- Arrays/arrays.js
+++ Arrays/arrays.js
@@ -1,54 +1,55 @@
 (function () {
     var array = [1, 5, 9, 88, 22, 48, 97, 1, 4, 55, 4, 9, 545];
-    console.log("Изначальный массив:");
-    console.log(array.toString());
 
-    array.sort(function (a, b) {
-        return b - a;
-    });
-
-    console.log();
-    console.log("Сортировка по убыванию:");
-    console.log(array.toString());
-
-    console.log();
-    console.log("Подмассив первых 5 элементов:");
-    console.log(array.slice(0, 5).toString());
-
-    console.log();
-    console.log("Подмассив последних 5 элементов:");
-    console.log(array.slice(-5).toString());
-
-    var result = array.reduce(function (sum, elem) {
-        if (elem % 2 === 0) {
-            sum += elem;
-        }
-        return sum;
-    }, 0);
-
-    console.log();
-    console.log("Сумма четных элементов массива:");
-    console.log(result);
-}());
-
-(function () {
-    var array1 = [];
-    for (var i = 1; i <= 100; i++) {
-        array1.push(i);
+    function sortDesc(array) {
+        return array.sort(function (a, b) {
+            return b - a;
+        });
     }
 
-    var array2 = array1.filter(function (elem) {
-        if (elem % 2 === 0) {
-            return true;
+    function getFirstSubarray(array) {
+        return array.slice(0, 5);
+    }
+
+    function getLastSubarray(array) {
+        return array.slice(-5);
+    }
+    
+    function getEvenSum(array) {
+        return array.reduce(function (sum, elem) {
+            if (elem % 2 === 0) {
+                sum += elem;
+            }
+
+            return sum;
+        }, 0);
+    }
+
+    function getArraySquaresEvenNumbers(array) {
+        var array = [];
+        for (var i = 1; i <= 100; i++) {
+            array.push(i);
         }
-    }).map(function (elem) {
-        return Math.pow(elem, 2);
-    });
 
-    console.log();
-    console.log("Массив квадратов четных чисел:");
-    console.log(array2.toString());
+        return array.filter(function (elem) {
+            return elem % 2 === 0;
+        }).map(function (elem) {
+            return Math.pow(elem, 2);
+        });
+    }
+
+    console.log("Сортировка по убыванию");
+    console.log(sortDesc(array).toString());
+
+    console.log("Подмассив первых 5 элементов");
+    console.log(getFirstSubarray(array).toString());
+
+    console.log("Подмассив последних 5 элементов");
+    console.log(getLastSubarray(array).toString());
+
+    console.log("Сумма четных элементов массива");
+    console.log(getEvenSum(array).toString());
+
+    console.log("Массив квадратов четных чисел");
+    console.log(getArraySquaresEvenNumbers(array).toString());
 }());
-
-
-
